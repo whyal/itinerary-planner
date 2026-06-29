fe5ba0f5a1fe800b3c2e4dc1ca2e06ae8e83896e
whyal <tydevsforfun@gmail.com>
2026-06-29 18:27:41 +0800

    Added web app dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,12 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-h2console")
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ src/main/java/com/yonglun/webportfolio/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.yonglun.webportfolio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Spring Boot!";
+    }
+}
