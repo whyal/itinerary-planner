d2cde47878d7fa1e37dc388712f1fbd9c56104ee
whyal <tydevsforfun@gmail.com>
2026-07-23 02:58:28 +0800

    Changed API endpoint.

--- src/main/java/com/yonglun/itineraryassistant/controller/ItineraryController.java
+++ src/main/java/com/yonglun/itineraryassistant/controller/ItineraryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/api/v1/itinerary")
+@RequestMapping("/api/itinerary")
 public class ItineraryController {
     private final ItineraryService itineraryService;
 
